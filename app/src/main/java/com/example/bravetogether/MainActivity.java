cab498bd54a367b4ffc19c718868bf73ecaa6a97
arbel_amir <arbel.amir@mail.huji.ac.il>
2019-02-22 12:50:56 +0200

    app merged

--- app/src/main/java/com/example/bravetogether/MainActivity.java
+++ app/src/main/java/com/example/bravetogether/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.bravetogether;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.testing.braveTogether.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
